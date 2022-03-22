05b2883d5f47236963976e44e0678d449a30cdfc
GunaRakulan-GR <gunarakulan@gmail.com>
2022-03-22 21:48:51 +0530

    Clean files

--- run-with-images.py
+++ run-with-images.py
@@ -4,7 +4,7 @@ import numpy as np
 
 faceModel = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-img = cv2.imread("image_4.jpg")
+img = cv2.imread("image-4.jpg")
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 faces = faceModel.detectMultiScale(gray,1.3,5)
 
