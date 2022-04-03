a1403f0b315f0c7c2dcbd57ae80f5363d22815c0
GunaRakulan-GR <gunarakulan@gmail.com>
2022-04-03 23:07:04 +0530

    Update code

--- 1-run-with-videos.py
+++ 1-run-with-videos.py
@@ -1,24 +1,30 @@
 import cv2
-import  numpy as np 
 
 faceModel = cv2.CascadeClassifier("haarcascade-frontalface-default.xml")
 
 video = cv2.VideoCapture("test-video.mp4")
 
 while True:
-	
-	ret,img = video.read()
-	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+	ret,frame = video.read()
+
+	if ret == False:
+		break
+
+	gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
 	faces = faceModel.detectMultiScale(gray,1.3,5)
 
 	for(x,y,w,h) in faces:
-		cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
-	
-	cv2.imshow("Video",img)
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),3)
 
+	cv2.imshow("Display", frame)
 
-	if(cv2.waitKey(1)==ord("q")):
+	if(cv2.waitKey(1) == ord("q")):
 		break
+
+
 video.release()
 cv2.destroyAllWindows()
+
+
